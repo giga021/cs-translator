879b50ab1f1cf240132005edf654cb7558451e2c
Giga <Giga@DESKTOP-MA71J2V>
2019-04-02 13:02:47 +0200

    Home page design

--- CsTranslator.Web/Controllers/HomeController.cs
+++ CsTranslator.Web/Controllers/HomeController.cs
@@ -5,19 +5,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CsTranslator.Web.Models;
+using Microsoft.Extensions.Options;
+using CsTranslator.Application;
 
 namespace CsTranslator.Web.Controllers
 {
 	public class HomeController : Controller
 	{
-		public IActionResult Index()
+		private readonly IOptions<AppSettings> _appSettings;
+
+		public HomeController(IOptions<AppSettings>appSettings)
 		{
-			return View();
+			_appSettings = appSettings;
 		}
 
-		public IActionResult Privacy()
+		public IActionResult Index()
 		{
-			return View();
+			var vm = new TranslateViewModel
+			{
+				SourceLanguage = _appSettings.Value.DefaultLanguageInput
+			};
+			return View(vm);
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ CsTranslator.Web/Models/TranslateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CsTranslator.Web.Models
+{
+	public class TranslateViewModel
+	{
+		public string SourceLanguage { get; set; }
+	}
+}
